7d0d8a3a6b0f08081d2a4376353d39ddf33f64dc
Arshil Amaan Ansari <99838974+arshil-amaan@users.noreply.github.com>
2023-03-22 01:35:05 +0530

    Update average2.java

--- average2.java
+++ average2.java
@@ -6,7 +6,7 @@ class average2 {
     for(int i=0; i<arr.length; i++){
       avg+=arr[i];
     }
-    System.out.println(avg/arr.length);
+    System.out.println("Average is :\n"+avg/arr.length);
 
 
   }
