65292b6814506297616417fed2c6fc83595e99c9
Arshil Amaan Ansari <99838974+arshil-amaan@users.noreply.github.com>
2023-03-25 00:35:49 +0530

    Update swap.java

--- swap.java
+++ swap.java
@@ -1,12 +1,12 @@
+// code to swap two integers
 import java.util.Scanner;
 public class swap {
   static int swapin(int a, int b){
-    int temp;
-    temp=a; a=b; b=temp;
+    int temp=a; a=b; b=temp;
     System.out.println("First num = "+a+", Second num = "+b+" (after function call)");
     return 0;
   }
-  public static void main(String [] args){
+  public static void main(String[] args){
     int num1=10,num2=45;
     Scanner sc = new Scanner(System.in);
     System.out.println("Enter First Number");
