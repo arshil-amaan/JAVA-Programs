74a4dc4c2a2e86603e8ada8ddd36b24bcaccface
Arshil Amaan Ansari <99838974+arshil-amaan@users.noreply.github.com>
2023-04-27 00:33:11 +0530

    Update average2.java

--- average2.java
+++ average2.java
@@ -3,6 +3,7 @@ class average2 {
   public static void main(String[] args){
     int arr[]={2,4,6,8};
     float avg=0;
+    //looping for average
     for(int i=0; i<arr.length; i++){
       avg+=arr[i];
     }
