593675b7511477779726c86f688e9818fe41a739
Arshil Amaan Ansari <99838974+arshil-amaan@users.noreply.github.com>
2023-04-27 00:37:34 +0530

    Update first_java.java

--- first_java.java
+++ first_java.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 class first_java {
     public static void main(String[] args) {
-    	
+    	//a scanner object is created to take input using objname.nextInt() / .nextFloat() / nextLine()
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter an integer: \n");
         int number = sc.nextInt();
