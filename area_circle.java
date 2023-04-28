fc3a50bf302d0ca682eacdefce8b48b1177438f6
Arshil Amaan Ansari <99838974+arshil-amaan@users.noreply.github.com>
2023-04-29 00:06:26 +0530

    Update area_circle.java

--- area_circle.java
+++ area_circle.java
@@ -1,7 +1,9 @@
 //code for area of circle (instance Variable)
 class area_circle {
+  // instance variable
   double r=10.5;
   public static void main(String []args){
+    // creating object to access instance variable
     area_circle myobj = new area_circle();
     System.out.println("Area of Circle is :\n"+Math.pow(myobj.r,2)*22/7);
   }
