5f76a58abda4baef69a43efcf5f724aa151caa89
Arshil Amaan Ansari <99838974+arshil-amaan@users.noreply.github.com>
2023-04-30 11:22:05 +0530

    Update simple_interest2.java

--- simple_interest2.java
+++ simple_interest2.java
@@ -3,6 +3,7 @@ class simple_interest2 {
   public static void main(String[] args){
     float si,p=250,r=15;
     int t = 3;
+    // using simple interest formula
     si=(p*r*t)/100;
     System.out.println("Simple Interest is :\n"+si);
   }
