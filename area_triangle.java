54639c19e23c912a73768002a219d5e4469aafe7
Arshil Amaan Ansari <99838974+arshil-amaan@users.noreply.github.com>
2023-05-02 00:24:52 +0530

    Update area_triangle.java

--- area_triangle.java
+++ area_triangle.java
@@ -1,5 +1,6 @@
  // code for Area of Triangle (static variables)
  class area_triangle {
+  // static variables
   static float b=5,h=7;
   public static void main(String[] args){
     System.out.println("Area of triangle is :\n"+(b*h)/2);
