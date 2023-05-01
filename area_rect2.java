de13aabe19e62558ae373ee7d85e011b26e377ab
Arshil Amaan Ansari <99838974+arshil-amaan@users.noreply.github.com>
2023-05-02 00:24:01 +0530

    Update area_rect2.java

--- area_rect2.java
+++ area_rect2.java
@@ -1,6 +1,7 @@
 //code for area of rectangle (local Variable)
 class area_rect2 {
   public static void main(String[] args){
+    // local variable
     int l=6;
     int b=5;
     System.out.println("Area of Rectangle is :\n"+l*b);
