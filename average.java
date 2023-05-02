9af16b1d7633a9a1697b30c5941513d7975a3258
Arshil Amaan Ansari <99838974+arshil-amaan@users.noreply.github.com>
2023-05-03 00:09:58 +0530

    Update average.java

--- average.java
+++ average.java
@@ -1,8 +1,10 @@
 //code for average (instance variable)
 class average {
+  // instance variables
   int len,arr[]={2,4,7,8};
   float sum=0;
   public static void main(String []args){
+    // Object created to access instance variables
     average myobj = new average();
     myobj.len=myobj.arr.length;
     for(int i=0;i<myobj.arr.length;i++){
