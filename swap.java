b944dd94ca9b49499b4013522969815312a33138
Arshil Amaan Ansari <99838974+arshil-amaan@users.noreply.github.com>
2023-05-04 00:13:42 +0530

    Update swap.java

--- swap.java
+++ swap.java
@@ -9,7 +9,7 @@ public class swap {
     return 0;
   }
   public static void main(String[] args){
-    int num1=10,num2=45;
+    int num1,num2;
     Scanner sc = new Scanner(System.in);
     System.out.println("Enter First Number");
     num1 = sc.nextInt();
