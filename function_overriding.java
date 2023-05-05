78478a3b805e6fad09b06169eac4ea1eb1681523
Arshil Amaan Ansari <99838974+arshil-amaan@users.noreply.github.com>
2023-05-06 00:36:54 +0530

    Update function_overriding.java

--- function_overriding.java
+++ function_overriding.java
@@ -1,23 +1,23 @@
 import java.util.*;
 
-class par{
+class A{
   int fun(int m){
-    System.out.println("defined in par");
+    System.out.println("defined in A class");
     return 2*m;
   }
 }
 
 class function_overriding{
   int fun(int n){
-    System.out.print("defined inside main ");
+    System.out.print("defined inside driver class ");
     return 3*n;
   }
   public static void main(String []args){
     Scanner sc = new Scanner(System.in);
-    par ob = new par();
+    A ob = new A();
     function_overriding obj = new function_overriding();
     ob.fun(7);
-    System.out.println("using\npar ob = new par();\nob.fun(7)");
+    System.out.println("using\nA ob = new A();\nob.fun(7)");
     System.out.println("****************************************");
     obj.fun(9);
     System.out.println("using\nfunction_overriding obj = new function_overriding();\nobj.fun(9);");
