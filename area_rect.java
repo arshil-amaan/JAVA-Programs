5092d497f8eeb1eefa561edca337a6fb911729a8
Arshil Amaan Ansari <99838974+arshil-amaan@users.noreply.github.com>
2023-05-07 00:11:25 +0530

    Update area_rect.java

--- area_rect.java
+++ area_rect.java
@@ -5,6 +5,7 @@ class area_rect {
   public static void main(String []args){
     // object created of same class
     area_rect myobj = new area_rect();
+    // printing area of rectangle
     System.out.println("Area of Rectangle is :\n"+myobj.l*myobj.b);
   }
 }
