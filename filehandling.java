1b1edd0fd14a5c0ac8ba92a74ec9f3c82f202082
Arshil Amaan Ansari <99838974+arshil-amaan@users.noreply.github.com>
2023-05-09 15:33:20 +0530

    Add files via upload

---
+++ filehandling.java
@@ -0,0 +1,61 @@
+package temporary;
+import java.util.*;
+import java.io.*;
+
+public class filehandling {
+  public static void main(String []args){
+    Scanner sc = new Scanner(System.in);
+    System.out.println("Enter file name with extension");
+    String file_name = sc.nextLine();
+    System.out.println("Enter data to write in file");
+    String data = sc.nextLine();
+    // creation of file, writing on file
+    try {
+      // object of File
+      File f = new File(file_name);
+      // creating new file using File object
+      f.createNewFile();
+      // ********************************************************
+      // object of FileWriter
+      FileWriter fw = new FileWriter(file_name);
+      // writing data on file
+      fw.write(data);
+      fw.close();
+    } catch (Exception e) {
+      System.out.println("Error is : "+e);
+    }
+    // **********************************************************
+    // read from file
+    try{
+      // object of File
+      File fr = new File(file_name);
+      // creating scanner object which takes input from file
+      Scanner file_content = new Scanner(fr);
+      // storing single line from file in variable
+      String file_data = file_content.nextLine();
+      // printing data from file
+      System.out.println(file_data);
+      // closing scanner
+      file_content.close();
+    } catch(Exception e) {
+      System.out.println("Error is : "+e);
+    }
+    // **********************************************************
+    // delete a file
+    try {
+      System.out.println("Enter file name with extension to delete");
+      String dlt_file = sc.nextLine();
+      // object of File
+      File f = new File(dlt_file);
+      try{
+        // deleting file
+        f.delete();
+      } catch(Exception e){
+        System.out.println("Error is : "+e);
+      }
+    } catch (Exception e) {
+      System.out.println("Error is : "+e);
+    }
+    sc.close();
+  }
+}
