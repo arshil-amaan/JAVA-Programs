1013ca6f0ed1ea56c68dbd6e2b097a55e831ab2a
Arshil Amaan Ansari <99838974+arshil-amaan@users.noreply.github.com>
2023-06-10 00:57:22 +0530

    Update area_rect.java

--- area_rect.java
+++ area_rect.java
@@ -1,11 +1,11 @@
 //code for area of rectangle (instance Variable)
 class area_rect {
-  // instance variables;
-  int l=4, b=9;
-  public static void main(String []args){
-    // object created of same class
-    area_rect myobj = new area_rect();
-    // printing area of rectangle
-    System.out.println("Area of Rectangle is :\n"+myobj.l*myobj.b);
-  }
+	// instance variables;
+	int l=4, b=9;
+	public static void main(String []args){
+		// object created of same class
+		area_rect myobj = new area_rect();
+		// printing area of rectangle
+		System.out.println("Area of Rectangle is :\n"+myobj.l*myobj.b);
+	}
 }
