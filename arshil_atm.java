a15fc3b829e48e51f6130d60bf4d19ec1e8a478b
Arshil Amaan Ansari <99838974+arshil-amaan@users.noreply.github.com>
2023-06-14 01:32:07 +0530

    Update arshil_atm.java

--- arshil_atm.java
+++ arshil_atm.java
@@ -103,14 +103,13 @@ public class arshil_atm {
 			card = sc.nextInt ();
 		}
 
-    while (lang != 1) {
-    	System.out.println ("Choose Language to Proceed");
-    	System.out.println ("English - 1\nHindi - 2");
-    	lang = sc.nextInt ();
-	    if (lang == 2)
-	        System.out.println("Currently Hindi Version Not Available. Kindly Choose English");
-					else if (lang != 1)
-    	  {
+		while (lang != 1) {
+			System.out.println ("Choose Language to Proceed");
+			System.out.println ("English - 1\nHindi - 2");
+			lang = sc.nextInt ();
+			if (lang == 2)
+				System.out.println("Currently Hindi Version Not Available. Kindly Choose English");
+			else if (lang != 1) {
     	    System.out.println ("Invalid Language Selected");
     	  }
       }
