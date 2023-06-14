189bc4f8208259720dd02c942a197626dd9894aa
Arshil Amaan Ansari <99838974+arshil-amaan@users.noreply.github.com>
2023-06-15 00:02:47 +0530

    Update average2.java

--- average2.java
+++ average2.java
@@ -1,13 +1,13 @@
 //code for average (local variable)
 class average2 {
-  public static void main(String[] args) {
-    // local variables
-    int arr[]={2,4,6,8};
-    float avg=0;
-    //looping for average
-    for(int i=0; i<arr.length; i++){
-      avg+=arr[i];
-    }
-    System.out.println("Average is :\n"+avg/arr.length);
-  }
+	public static void main(String[] args) {
+		// local variables
+		int arr[]={2,4,6,8};
+		float avg=0;
+		//looping for average
+		for(int i=0; i<arr.length; i++){
+			avg+=arr[i];
+		}
+		System.out.println("Average is :\n"+avg/arr.length);
+	}
 }
