15e92ebe6a0a3823f6161207aa3a27407d7b9c2f
Arshil Amaan Ansari <99838974+arshil-amaan@users.noreply.github.com>
2023-06-15 00:07:06 +0530

    Update exception_handling.java

--- exception_handling.java
+++ exception_handling.java
@@ -17,24 +17,24 @@ class exception_handling{
 // 			}
 		return c;
 	}
-  public static void main(String []args){
-    Scanner sc = new Scanner(System.in);
-    exception_handling obj = new exception_handling();
+	public static void main(String []args){
+		Scanner sc = new Scanner(System.in);
+		exception_handling obj = new exception_handling();
 
-    int p,q;
-    System.out.println("Enter First Num");
-    p=sc.nextInt();
-    System.out.println("Enter Second Num");
-    q=sc.nextInt();
-    obj.division(p,q);
+		int p,q;
+		System.out.println("Enter First Num");
+		p=sc.nextInt();
+		System.out.println("Enter Second Num");
+		q=sc.nextInt();
+		obj.division(p,q);
 
-    System.out.println("********************");
-    
-    int arr[]={2,4,6,8,3};
-    
-    System.out.println(arr[3]);
-    
-    try{
+		System.out.println("********************");
+
+		int arr[]={2,4,6,8,3};
+
+		System.out.println(arr[3]);
+
+		try{
       System.out.println(arr[7]);
     }
     
