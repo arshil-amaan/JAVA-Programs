29ce27e5d46f4451eb44ef33f253223913451ee8
Arshil Amaan Ansari <99838974+arshil-amaan@users.noreply.github.com>
2023-06-17 04:19:40 +0530

    Create factorial.java

---
+++ factorial.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class factorial {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter no. : ");
+		int g=sc.nextInt();
+        int fact = 1;
+		System.out.println();
+		for(int i = 1; i<=g; i++) {
+			fact*=i;
+		}
+        System.out.println(fact);
+        sc.close();
+	}
+}
