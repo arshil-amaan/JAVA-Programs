ac83d9fe3aed1725e6e4915884964895fd39a54a
Arshil Amaan Ansari <99838974+arshil-amaan@users.noreply.github.com>
2023-06-19 00:33:17 +0530

    Update and rename Sub_Arrays.java to sub_Arrays.java

--- sub_Arrays.java
+++ sub_Arrays.java
@@ -1,14 +1,19 @@
 import java.util.Scanner;
 
-public class Sub_Arrays {
+public class sub_Arrays {
   static void sub_arrays(int[] myArray) {
+    int totSA = 0;
     for(int i = 0; i < myArray.length ; i++){
       for(int j = i; j < myArray.length ; j++){
         for(int k = i ; k<=j ; k++){
           System.out.print(myArray[k]+" ");
-        }System.out.print("\t");
-      }System.out.println();
+        }
+        System.out.println();
+        totSA++;
+      }
+	  System.out.println("\n");
     }
+    System.out.println("Total no. of sub arrays : "+totSA);
   }
   public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
