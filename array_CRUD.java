f8974998f284d447deb7e10d949c86e5cafde91d
Arshil Amaan Ansari <99838974+arshil-amaan@users.noreply.github.com>
2023-06-18 18:33:11 +0530

    Update and rename array CRUD.java to array_CRUD.java

---
+++ array_CRUD.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class ArrayCRUD {
+    static void update(int[] myArray) {
+        Scanner in = new Scanner(System.in);
+        System.out.println("\nPreviously:");
+        for (int i = 0; i < myArray.length; i++) {
+          System.out.print(myArray[i] + " ");
+        }
+
+        System.out.println("\n\nEnter new values:");
+
+        for (int i = 0; i < myArray.length; i++) {
+          myArray[i] = in.nextInt();
+        }
+
+        System.out.println("\nUpdated:");
+
+        for (int i = 0; i < myArray.length; i++) {
+          System.out.print(myArray[i] + " ");
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int[] arr = new int[5];
+        update(arr);
+
+        int[] brr = {3, 4, 5, 1};
+        update(brr);
+        sc.close();
+    }
+}
