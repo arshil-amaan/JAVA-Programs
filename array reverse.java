3c959d82e3da663e756af84cab818e76f1acab11
Arshil Amaan Ansari <99838974+arshil-amaan@users.noreply.github.com>
2023-06-18 23:52:34 +0530

    Create array reverse.java

---
+++ array reverse.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class ArrayCRUD {
+  static void reverse(int[] myArray) {
+    for(int i = 0; i < myArray.length/2 ; i++){
+      int temp = myArray[i];
+      myArray[i] = myArray[myArray.length-1-i];
+      myArray[myArray.length-1-i] = temp;
+    }
+  }
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    int arr[] = {1, 7, 8, 5, 6};
+    System.out.println("\nPreviously :");
+    for (int i = 0; i < arr.length; i++) {
+      System.out.print(arr[i] + " ");
+    }
+    reverse(arr);
+
+    System.out.println("\nNow :");
+    for (int i = 0; i < arr.length; i++) {
+      System.out.print(arr[i] + " ");
+    }
+    sc.close();
+  }
+}
