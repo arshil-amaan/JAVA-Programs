e30741e69abda83631f074b83b9150506a455b52
Arshil Amaan Ansari <99838974+arshil-amaan@users.noreply.github.com>
2023-06-18 21:48:52 +0530

    Create highest and lowest of data types.java

---
+++ highest and lowest of data types.java
@@ -0,0 +1,19 @@
+public class high_low_data_types{
+  public static void main(String[] args){
+  	double[] brr = {
+          Byte.MAX_VALUE,
+          Byte.MIN_VALUE,
+          Integer.MAX_VALUE,
+          Integer.MIN_VALUE,
+          Long.MAX_VALUE,
+          Long.MIN_VALUE,
+          Float.MAX_VALUE,
+          Float.MIN_VALUE,
+          Double.MAX_VALUE,
+          Double.MIN_VALUE
+        };
+        for (int i = 0; i < brr.length; i++) {
+          System.out.print(brr[i] + "\n");
+        }
+  }
+}
