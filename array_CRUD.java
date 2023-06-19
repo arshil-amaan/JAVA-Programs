29a020b7c18c8fac833a6a9e7cf897763844f0a8
Arshil Amaan Ansari <99838974+arshil-amaan@users.noreply.github.com>
2023-06-20 00:21:02 +0530

    Update array_CRUD.java

--- array_CRUD.java
+++ array_CRUD.java
@@ -1,25 +1,25 @@
 import java.util.Scanner;
 
 public class ArrayCRUD {
-    static void update(int[] myArray) {
-        Scanner in = new Scanner(System.in);
-        System.out.println("\nPreviously:");
-        for (int i = 0; i < myArray.length; i++) {
-          System.out.print(myArray[i] + " ");
-        }
-
-        System.out.println("\n\nEnter new values:");
-
-        for (int i = 0; i < myArray.length; i++) {
-          myArray[i] = in.nextInt();
-        }
-
-        System.out.println("\nUpdated:");
-
-        for (int i = 0; i < myArray.length; i++) {
-          System.out.print(myArray[i] + " ");
-        }
-    }
+	static void update(int[] myArray) {
+		Scanner in = new Scanner(System.in);
+		System.out.println("\nPreviously:");
+		for (int i = 0; i < myArray.length; i++) {
+			System.out.print(myArray[i] + " ");
+		}
+		
+		System.out.println("\n\nEnter new values:");
+		
+		for (int i = 0; i < myArray.length; i++) {
+			myArray[i] = in.nextInt();
+		}
+		
+		System.out.println("\nUpdated:");
+		
+		for (int i = 0; i < myArray.length; i++) {
+			System.out.print(myArray[i] + " ");
+		}
+	}
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
