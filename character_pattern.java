fab5e361437f564dee962d26397cdf1ba594dec1
Arshil Amaan Ansari <99838974+arshil-amaan@users.noreply.github.com>
2023-06-19 10:42:34 +0530

    Update character_pattern.java

--- character_pattern.java
+++ character_pattern.java
@@ -1,5 +1,5 @@
 import java.util.*;
-public class Solution {
+public class character_pattern {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		System.out.print("Enter no. of lines : ");
