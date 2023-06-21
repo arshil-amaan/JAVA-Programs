65ceba48485467e0887e797f31f9a8cfc5323f27
Arshil Amaan Ansari <99838974+arshil-amaan@users.noreply.github.com>
2023-06-22 00:48:30 +0530

    Update first_java.java

--- first_java.java
+++ first_java.java
@@ -1,14 +1,14 @@
 import java.util.Scanner;
 // driver class
 class first_java {
-    public static void main(String[] args) {
-    	//a scanner object is created to take input using objname.nextInt() / .nextFloat() / .nextLine() / .next()
-        Scanner sc = new Scanner(System.in);
-        System.out.print("Enter an integer: \n");
-        // taking user input
-        int number = sc.nextInt();
-        System.out.print("You entered " + number);
-        // it closes sc object of Scanner class, otherwise warning may occur
-        sc.close();
-    }
+	public static void main(String[] args) {
+		//a scanner object is created to take input using objname.nextInt() / .nextFloat() / .nextLine() / .next()
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter an integer: \n");
+		// taking user input
+		int number = sc.nextInt();
+		System.out.print("You entered " + number);
+		// it closes sc object of Scanner class, otherwise warning may occur
+		sc.close();
+	}
 }
