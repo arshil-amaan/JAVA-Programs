556cb02141e3dcb796a73ab0b67f302135942813
Arshil Amaan Ansari <99838974+arshil-amaan@users.noreply.github.com>
2023-06-23 23:57:34 +0530

    Update function_overriding.java

--- function_overriding.java
+++ function_overriding.java
@@ -14,15 +14,15 @@ class function_overriding {
 		// this function returns triple of n
 		return 3*n;
 	}
-  public static void main(String []args){
-    Scanner sc = new Scanner(System.in);
-    A ob = new A();
-    function_overriding obj = new function_overriding();
-    ob.fun(7);
-    System.out.println("using\nA ob = new A();\nob.fun(7)");
-    System.out.println("****************************************");
-    obj.fun(9);
-    System.out.println("using\nfunction_overriding obj = new function_overriding();\nobj.fun(9);");
-    sc.close();
-  }
+	public static void main(String []args){
+		Scanner sc = new Scanner(System.in);
+		A ob = new A();
+		function_overriding obj = new function_overriding();
+		ob.fun(7);
+		System.out.println("using\nA ob = new A();\nob.fun(7)");
+		System.out.println("****************************************");
+		obj.fun(9);
+		System.out.println("using\nfunction_overriding obj = new function_overriding();\nobj.fun(9);");
+		sc.close();
+	}
 }
