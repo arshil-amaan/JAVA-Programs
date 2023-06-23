dfed45bbec49c8b030bae0f870ee48c5f7389973
Arshil Amaan Ansari <99838974+arshil-amaan@users.noreply.github.com>
2023-06-23 23:59:32 +0530

    Update kadanes_maxsum_subarray.java

--- kadanes_maxsum_subarray.java
+++ kadanes_maxsum_subarray.java
@@ -16,24 +16,24 @@ public class kadanes_maxsum_subarray {
 		System.out.println("\nMaximum sub array is: "+maxis);
 		return maxis;
 	}
-public static void main( String[] args){
-Scanner sc = new Scanner(System.in);
-int size = 0;
-System.out.print("Enter Size: ");
-size = sc.nextInt();
-
-int arr[] = new int[size];
-
-System.out.print("Enter Elements: ");
-for(int i = 0 ; i<size ; i++){
-arr[i] = sc.nextInt();
-}
-
-System.out.print("Array is: ");
-for(int i = 0 ; i<size ; i++){
-System.out.print(arr[i]+" ");
-}
-
-kadanes_maxsum(arr);
-}
+	public static void main( String[] args){
+		Scanner sc = new Scanner(System.in);
+		int size = 0;
+		System.out.print("Enter Size: ");
+		size = sc.nextInt();
+		
+		int arr[] = new int[size];
+		
+		System.out.print("Enter Elements: ");
+		for(int i = 0 ; i<size ; i++) {
+			arr[i] = sc.nextInt();
+		}
+		
+		System.out.print("Array is: ");
+		for(int i = 0 ; i<size ; i++) {
+			System.out.print(arr[i]+" ");
+		}
+		
+		kadanes_maxsum(arr);
+	}
 }
