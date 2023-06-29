64d64f6f127ddb7551ec4865d4b106dd6c075f8b
Arshil Amaan Ansari <99838974+arshil-amaan@users.noreply.github.com>
2023-06-30 00:24:17 +0530

    Update multi_lvl_inherit_def_val_change.java

--- multi_lvl_inherit_def_val_change.java
+++ multi_lvl_inherit_def_val_change.java
@@ -1,27 +1,27 @@
 import java.util.Scanner;
 
 class a {
-  int a=4,b=7;
+	int a=4,b=7;
 }
 
 class b extends a {
-  int c=a*b;
-  void bcla() {
-    c=a*b;
-  }
+	int c=a*b;
+	void bcla() {
+		c=a*b;
+	}
 }
 
 public class multi_lvl_inherit_def_val_change extends a {
   public static void main(String[] args) {
-    Scanner sc = new Scanner(System.in);
-    b ob = new b();
-    System.out.println("type a : ");
-    ob.a = sc.nextInt();
-    System.out.println("type b : ");
-    ob.b = sc.nextInt();
-    System.out.println("prod is (using default values): "+ob.c);
-    ob.bcla();
-    System.out.println("prod is (using fun (actually change)): "+ob.c);
-    sc.close();
+		Scanner sc = new Scanner(System.in);
+		b ob = new b();
+		System.out.println("type a : ");
+		ob.a = sc.nextInt();
+		System.out.println("type b : ");
+		ob.b = sc.nextInt();
+		System.out.println("prod is (using default values): "+ob.c);
+		ob.bcla();
+		System.out.println("prod is (using fun (actually change)): "+ob.c);
+		sc.close();
   }
 }
