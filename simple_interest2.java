99cbe59176c9635bba37230028086dd1998e503a
Arshil Amaan Ansari <99838974+arshil-amaan@users.noreply.github.com>
2023-07-02 02:06:33 +0530

    Update simple_interest2.java

--- simple_interest2.java
+++ simple_interest2.java
@@ -1,10 +1,10 @@
 // code for simple interest (local variable)
 class simple_interest2 {
 	public static void main(String[] args) {
-	float si,p=250,r=15;
-	int t = 3;
-	// using simple interest formula
-	si=(p*r*t)/100;
-	System.out.println("Simple Interest is :\n"+si);
+		float si,p=250,r=15;
+		int t = 3;
+		// using simple interest formula
+		si=(p*r*t)/100;
+		System.out.println("Simple Interest is :\n"+si);
 	}
 }
