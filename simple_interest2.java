70611845de69312132665a6b89c56832e256c0d4
Arshil Amaan Ansari <99838974+arshil-amaan@users.noreply.github.com>
2023-07-02 02:05:18 +0530

    Update simple_interest2.java

--- simple_interest2.java
+++ simple_interest2.java
@@ -1,6 +1,6 @@
 // code for simple interest (local variable)
 class simple_interest2 {
-  public static void main(String[] args){
+  public static void main(String[] args) {
     float si,p=250,r=15;
     int t = 3;
     // using simple interest formula
