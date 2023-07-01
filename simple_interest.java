c3b40399d8bb0c961ed3040142ccc996474f27a1
Arshil Amaan Ansari <99838974+arshil-amaan@users.noreply.github.com>
2023-07-02 02:04:46 +0530

    Update simple_interest.java

--- simple_interest.java
+++ simple_interest.java
@@ -1,13 +1,13 @@
 // code for simple interest (instance variable)
 class simple_interest {
-  // instance variables
-  float si,p=250,r=15;
-  int t=3;
-  // main function
-  public static void main(String[] args){
-//     driver class object
-    simple_interest myobj = new simple_interest();
-    myobj.si=((myobj.p*myobj.r*myobj.t)/100);
-    System.out.println("Simple Interest is :\n"+myobj.si);
-  }
+	// instance variables
+	float si,p=250,r=15;
+	int t=3;
+	// main function
+	public static void main(String[] args){
+		//     driver class object
+		simple_interest myobj = new simple_interest();
+		myobj.si=((myobj.p*myobj.r*myobj.t)/100);
+		System.out.println("Simple Interest is :\n"+myobj.si);
+	}
 }
