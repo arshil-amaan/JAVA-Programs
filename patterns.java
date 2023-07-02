81915ae6fcaead41ca14da56987857d32758e356
Arshil Amaan Ansari <99838974+arshil-amaan@users.noreply.github.com>
2023-07-03 01:12:36 +0530

    Update patterns.java

--- patterns.java
+++ patterns.java
@@ -18,27 +18,27 @@ public class patterns {
 		}
 	}
 
-  static void hollow_square_wall(){
-    // * * * * *
-    // *       *
-    // *       *
-    // *       *
-    // * * * * *
-    Scanner sc = new Scanner(System.in);
-    System.out.print("Enter no. of lines : ");
-    int limit=sc.nextInt();
-    System.out.println();
-    for(int i = 0; i<limit; i++) {
-      for(int j = 0 ; j<limit ; j++){
-        if(i==0 || i==limit-1 || j==0 || j==limit-1){
-          System.out.print("* ");
-        } else {
-          System.out.print("  ");
-        }
-      }
-      System.out.println();
-    }
-  }
+	static void hollow_square_wall() {
+		// * * * * *
+		// *       *
+		// *       *
+		// *       *
+		// * * * * *
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter no. of lines : ");
+		int limit=sc.nextInt();
+		System.out.println();
+		for(int i = 0; i<limit; i++) {
+			for(int j = 0 ; j<limit ; j++){
+				if(i==0 || i==limit-1 || j==0 || j==limit-1){
+					System.out.print("* ");
+				} else {
+					System.out.print("  ");
+				}
+			}
+			System.out.println();
+		}
+	}
   
   static void skew_square(){
 	//         * * * * *
