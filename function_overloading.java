88c4bf652ef5be9685c56225f1e12227acaff877
Arshil Amaan Ansari <99838974+arshil-amaan@users.noreply.github.com>
2023-07-03 01:08:25 +0530

    Update function_overloading.java

--- function_overloading.java
+++ function_overloading.java
@@ -15,14 +15,14 @@ public class function_overloading {
 		return (a/b);
 	}
 
-  public static void main(String[] args) {
-    // object created of driver class
-    function_overloading obj = new function_overloading();
-    //multiplied because 2 arguments passed
-    System.out.println(obj.calc(3,6));
-    // added because 3 arguments passed
-    System.out.println(obj.calc(2,5,8));
-    // divided because 2 double arguments passed
-    System.out.println(obj.calc(18.0,6.0));
-  }
+	public static void main(String[] args) {
+		// object created of driver class
+		function_overloading obj = new function_overloading();
+		//multiplied because 2 arguments passed
+		System.out.println(obj.calc(3,6));
+		// added because 3 arguments passed
+		System.out.println(obj.calc(2,5,8));
+		// divided because 2 double arguments passed
+		System.out.println(obj.calc(18.0,6.0));
+	}
 }
