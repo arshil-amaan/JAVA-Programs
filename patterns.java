fe74251f3a9b945058866a33be3e87b60f2c82ee
Arshil Amaan Ansari <99838974+arshil-amaan@users.noreply.github.com>
2023-07-04 03:48:44 +0530

    Update patterns.java

--- patterns.java
+++ patterns.java
@@ -30,7 +30,7 @@ public class patterns {
 		System.out.println();
 		for(int i = 0; i<limit; i++) {
 			for(int j = 0 ; j<limit ; j++) {
-				if(i==0 || i==limit-1 || j==0 || j==limit-1){
+				if(i==0 || i==limit-1 || j==0 || j==limit-1) {
 					System.out.print("* ");
 				} else {
 					System.out.print("  ");
