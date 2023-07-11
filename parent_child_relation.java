189e1353fd87222e1aa3b7ae2eb8a7fb32a85539
Arshil Amaan Ansari <99838974+arshil-amaan@users.noreply.github.com>
2023-07-12 01:11:51 +0530

    Update parent_child_relation.java

--- parent_child_relation.java
+++ parent_child_relation.java
@@ -23,31 +23,31 @@ class child extends parent {
 }
 
 public class parent_child_relation {
-  public static void main(String[] args) {
-    parent o = new parent();
-    child b = new child();
-    parent j = new child();
-
-    System.out.println(o.d);
-    // System.out.println(o.t);    // Parent cannot access child data members
-    System.out.println(o.print());
-    System.out.println(o.desc_print_par());
-    // System.out.println(o.desc_print());   // Parent cannot access child member functions
-
-    System.out.println("******************************");
-    
-    System.out.println(b.t);
-    System.out.println(b.d);
-    System.out.println(b.print());
-    System.out.println(b.desc_print_par());
-    System.out.println(b.desc_print());
-    
-    System.out.println("******************************");
-
-    System.out.println(j.d);
-    // System.out.println(j.t);    // Parent cannot access child data members
-    System.out.println(j.print());
-    System.out.println(j.desc_print_par());
-    // System.out.println(j.desc_print());   // Parent cannot access child member functions
-  }
+	public static void main(String[] args) {
+		parent o = new parent();
+		child b = new child();
+		parent j = new child();
+		
+		System.out.println(o.d);
+		// System.out.println(o.t);    // Parent cannot access child data members
+		System.out.println(o.print());
+		System.out.println(o.desc_print_par());
+		// System.out.println(o.desc_print());   // Parent cannot access child member functions
+		
+		System.out.println("******************************");
+		
+		System.out.println(b.t);
+		System.out.println(b.d);
+		System.out.println(b.print());
+		System.out.println(b.desc_print_par());
+		System.out.println(b.desc_print());
+		
+		System.out.println("******************************");
+		
+		System.out.println(j.d);
+		// System.out.println(j.t);    // Parent cannot access child data members
+		System.out.println(j.print());
+		System.out.println(j.desc_print_par());
+		// System.out.println(j.desc_print());   // Parent cannot access child member functions
+	}
 }
