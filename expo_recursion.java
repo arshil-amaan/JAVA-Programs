b2cf7e21ce4d0331dc3ec268f834ad6d98d2f2ae
Arshil Amaan Ansari <99838974+arshil-amaan@users.noreply.github.com>
2023-07-17 23:06:42 +0530

    Add files via upload

---
+++ expo_recursion.java
@@ -0,0 +1,22 @@
+public class expo_recursion {
+  static int exponant(int n, int ex){
+    
+    // base case
+    if(ex==1){
+      return n;
+    }
+    
+    // to handle with negative power
+    if(ex<1){
+      return -1;
+    }
+
+    return n*exponant(n, ex-1);
+  }
+  public static void main(String[] args) {
+    int n = 3;
+    int ex = -6;
+
+    System.out.println(exponant(n,ex));
+  }
+}
