e8dc660c3f9f85784d201f6e62d370e0e8feaf33
Arshil Amaan Ansari <99838974+arshil-amaan@users.noreply.github.com>
2023-07-19 02:45:51 +0530

    Update area_circle2.java

--- area_circle2.java
+++ area_circle2.java
@@ -4,7 +4,7 @@ class area_circle2 {
 	public static void main(String[] args){
 		Scanner scs = new Scanner(System.in);
 		// local variable
-		double r=10.5;
+		double r=scs.nextInt();
 		// printing area of Circle
 		System.out.println("Area of Circle is :\n"+Math.pow(r,2)*22/7);
 	} 
