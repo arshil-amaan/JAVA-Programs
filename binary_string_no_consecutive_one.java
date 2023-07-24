d4a1770548728c4de0433926dcde0aaff6e0f1de
Arshil Amaan Ansari <99838974+arshil-amaan@users.noreply.github.com>
2023-07-25 01:00:26 +0530

    Add files via upload

---
+++ binary_string_no_consecutive_one.java
@@ -0,0 +1,17 @@
+public class binary_string_no_consecutive_one {
+  static void binStrs(int n,int lastPlace, String str){
+    if(n==0){
+      System.out.println(str);
+      return;
+    }
+
+    binStrs(n-1, 0, str+"0");
+    if(lastPlace==0){
+      binStrs(n-1, 1, str+"1");
+    }
+  }
+  public static void main(String[] args) {
+    int n = 3;
+    binStrs(n,0,"");
+  }
+}
