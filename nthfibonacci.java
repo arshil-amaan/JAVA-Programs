07dd33cb753516b013d2aac09f0cfe2406d67721
Arshil Amaan Ansari <99838974+arshil-amaan@users.noreply.github.com>
2023-07-27 00:02:11 +0530

    Update nthfibonacci.java

--- nthfibonacci.java
+++ nthfibonacci.java
@@ -2,7 +2,7 @@ public class nthfibonacci {
 	static int nthfibo(int n){
 		if(n==1) {
 			return 0;
-		} else if(n==2){
+		} else if(n==2) {
 			return 1;
 		} else { 
 			return nthfibo(n-1)+nthfibo(n-2);
