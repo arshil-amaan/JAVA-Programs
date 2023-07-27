18008c6fe00529eaa48aeb4f2daea62624dc7b22
Arshil Amaan Ansari <99838974+arshil-amaan@users.noreply.github.com>
2023-07-28 03:22:07 +0530

    Update bit_manipulation.java

--- bit_manipulation.java
+++ bit_manipulation.java
@@ -25,30 +25,30 @@ public class bit_manipulation {
 		}
 	}
 
-  static void clearIbits(int n, int i) {
-    // make last i bits 0
-    int bitMask = (~0 << i); // 1111 1000
-    System.out.println(n & bitMask);
-  }
+	static void clearIbits(int n, int i) {
+		// make last i bits 0
+		int bitMask = (~0 << i); // 1111 1000
+		System.out.println(n & bitMask);
+	}
 
-  static void clearBitRange(int n, int i, int j) {
-    // make i to j bits 0
-    int bitMask = (~0 << j) | (1 << i) - 1; // 1100 0111
-    System.out.println(n & (~0 << j) | ~(~0 << i));
-    System.out.println(n & (~0 << j) | (1 << i) - 1);
-    System.out.println(n & bitMask);
-  }
+	static void clearBitRange(int n, int i, int j) {
+		// make i to j bits 0
+		int bitMask = (~0 << j) | (1 << i) - 1; // 1100 0111
+		System.out.println(n & (~0 << j) | ~(~0 << i));
+		System.out.println(n & (~0 << j) | (1 << i) - 1);
+		System.out.println(n & bitMask);
+	}
 
-  public static void main(String[] args) {
-    // int n = 10; // 1010
-    int n = 2515; // 100111010011
-    int i = 3;
-    int j = 5;
-    getIthbit(n, i);
-    setIthbit(n, i);
-    clearIthbit(n, i);
-    updateIthbit(n, i, 1);
-    clearIbits(n, i);
-    clearBitRange(n, i, j);
-  }
+	public static void main(String[] args) {
+		// int n = 10; // 1010
+		int n = 2515; // 100111010011
+		int i = 3;
+		int j = 5;
+		getIthbit(n, i);
+		setIthbit(n, i);
+		clearIthbit(n, i);
+		updateIthbit(n, i, 1);
+		clearIbits(n, i);
+		clearBitRange(n, i, j);
+	}
 }
