1addd4130e007a933b6f136512e7f2bf0ee17db0
Arshil Amaan Ansari <99838974+arshil-amaan@users.noreply.github.com>
2023-08-01 00:57:19 +0530

    Update str_permutation.java

--- str_permutation.java
+++ str_permutation.java
@@ -1,18 +1,18 @@
 public class str_permutation {
-  static void strPermutations(String s, String ans){
-    if(s.length()==0){
-      System.out.println(ans);
-      return;
-    }
-
-    for (int i = 0; i < s.length(); i++) {
-      char ch= s.charAt(i);
-      String newStr = s.substring(0, i) + s.substring(i+1);
-      strPermutations(newStr, ans+ch);
-    }
-  }
-  public static void main(String[] args) {
-    String s = "abcd";
-    strPermutations(s,"");
-  }
+	static void strPermutations(String s, String ans) {
+		if(s.length()==0){
+			System.out.println(ans);
+			return;
+		}
+	
+		for (int i = 0; i < s.length(); i++) {
+			char ch= s.charAt(i);
+			String newStr = s.substring(0, i) + s.substring(i+1);
+			strPermutations(newStr, ans+ch);
+		}
+	}
+	public static void main(String[] args) {
+		String s = "abcd";
+		strPermutations(s,"");
+	}
 }
