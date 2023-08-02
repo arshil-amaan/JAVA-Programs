294ab597af8c94eac33f74db31580b52d8ff9f54
Arshil Amaan Ansari <99838974+arshil-amaan@users.noreply.github.com>
2023-08-03 00:01:08 +0530

    Update buynsellstock.java

--- buynsellstock.java
+++ buynsellstock.java
@@ -1,22 +1,22 @@
 import java.util.*;
 
 public class buynsellstock {
-  static int maxprof(int arr[]) {
-    int profit = 0;
-    int rightmax[] = new int[arr.length];
-    rightmax[arr.length - 1] = arr[arr.length - 1];
-    for (int i = rightmax.length - 2; i > -1; i--) {
-      rightmax[i] = Math.max(arr[i], rightmax[i + 1]);
-    }
-
-    for (int i = 0; i < arr.length; i++) {
-      if (rightmax[i] - arr[i] > profit) {
-        profit = rightmax[i] - arr[i];
-      }
-    }
-
-    return profit;
-  }
+	static int maxprof(int arr[]) {
+		int profit = 0;
+		int rightmax[] = new int[arr.length];
+		rightmax[arr.length - 1] = arr[arr.length - 1];
+		for (int i = rightmax.length - 2; i > -1; i--) {
+			rightmax[i] = Math.max(arr[i], rightmax[i + 1]);
+		}
+		
+		for (int i = 0; i < arr.length; i++) {
+			if (rightmax[i] - arr[i] > profit) {
+				profit = rightmax[i] - arr[i];
+			}
+		}
+		
+		return profit;
+	}
 
   public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
