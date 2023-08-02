150903562c2660f32ca1f28daff3cb6f8c702005
Arshil Amaan Ansari <99838974+arshil-amaan@users.noreply.github.com>
2023-08-03 00:01:36 +0530

    Update buynsellstock.java

--- buynsellstock.java
+++ buynsellstock.java
@@ -18,18 +18,18 @@ public class buynsellstock {
 		return profit;
 	}
 
-  public static void main(String[] args) {
-    Scanner sc = new Scanner(System.in);
-
-    System.out.print("Enter number of elements : ");
-    int n = sc.nextInt();
-    int arr[] = new int[n];
-    
-    System.out.println("Enter elements :");
-    for (int i = 0; i < n; i++) {
-      arr[i] = sc.nextInt();
-    }
-    
-    System.out.println(maxprof(arr));
-  }
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("Enter number of elements : ");
+		int n = sc.nextInt();
+		int arr[] = new int[n];
+		
+		System.out.println("Enter elements :");
+		for (int i = 0; i < n; i++) {
+			arr[i] = sc.nextInt();
+		}
+		
+		System.out.println(maxprof(arr));
+	}
 }
