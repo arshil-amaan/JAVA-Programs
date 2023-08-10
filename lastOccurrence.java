a5fe207b91754bf50ca72ec7ba9a7cb11762bc69
Arshil Amaan Ansari <99838974+arshil-amaan@users.noreply.github.com>
2023-08-10 22:50:16 +0530

    Update lastOccurrence.java

--- lastOccurrence.java
+++ lastOccurrence.java
@@ -1,11 +1,11 @@
 public class lastOccurrence {
 	static int findLastIndexOf(int[] arr,int key, int i) {
-	if(i==arr.length){
+	if(i==arr.length) {
 	return -1;
 	}
 	
 	int foundAt = findLastIndexOf(arr, key, i+1);
-	if(foundAt==-1 && arr[i]==key){
+	if(foundAt==-1 && arr[i]==key) {
 	return i;
 	}
 	
