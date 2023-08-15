c5b99bf95de60be17952baf3532ccfa37c9fbb01
Arshil Amaan Ansari <99838974+arshil-amaan@users.noreply.github.com>
2023-08-16 00:11:40 +0530

    Add files via upload

---
+++ StockSpan.java
@@ -0,0 +1,36 @@
+import java.util.*;
+
+public class StockSpan {
+  public static void stockSpan(int[] stocks, int[] span) {
+    Stack<Integer> s = new Stack<>();
+    span[0] = 1;
+    s.push(0);
+    for (int i = 1; i < stocks.length; i++) {
+      int currPrice = stocks[i];
+      while (!s.empty() && currPrice > stocks[s.peek()]) {
+        s.pop();
+      }
+      if (s.empty()) {
+        span[i] = i + 1;
+      } else {
+        int prevHigh = s.peek();
+        span[i] = i - prevHigh;
+      }
+      s.push(i);
+    }
+  }
+
+  public static void main(String[] args) {
+    int[] stocks = { 100, 80, 60, 70, 60, 85, 100 };
+    int[] span = new int[stocks.length];
+    stockSpan(stocks, span);
+    for (int i = 0; i < stocks.length; i++) {
+      System.out.print(stocks[i]+" ");
+    }
+    System.out.println();
+    for (int i = 0; i < span.length; i++) {
+      System.out.print(span[i] + " ");
+    }
+
+  }
+}
