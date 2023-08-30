3571b987a70922ac84d352871956d25665ac59df
Arshil Amaan Ansari <99838974+arshil-amaan@users.noreply.github.com>
2023-08-31 02:47:28 +0530

    Add files via upload

--- IndianCoins.java
+++ IndianCoins.java
@@ -2,23 +2,23 @@ public class IndianCoins {
   public static int countCoins(int num) {
     int[] denominations = { 1, 2, 5, 10, 20, 50, 100, 500, 2000 };
     int coins = 0;
-    
+
     // int i = denominations.length - 1;
     // while (num != 0) {
-    //   if (num >= denominations[i]) {
-    //     coins += num / denominations[i];
-    //     // System.out.print(denominations[i] + "x" + num / denominations[i] + " = " + denominations[i] * num / denominations[i] + " ");
-    //     num = num % denominations[i];
-    //     // System.out.println("=> remaining: " + num);
-    //   }
-    //   i--;
+    // if (num >= denominations[i]) {
+    // coins += num / denominations[i];
+    // // System.out.print(denominations[i] + "x" + num / denominations[i] + " = " +
+    // denominations[i] * num / denominations[i] + " ");
+    // num = num % denominations[i];
+    // // System.out.println("=> remaining: " + num);
+    // }
+    // i--;
     // }
 
     for (int i = denominations.length - 1; i > -1; i--) {
       if (num >= denominations[i]) {
         coins += num / denominations[i];
-        // System.out.print(denominations[i] + "x" + num / denominations[i] + " = " +
-        // denominations[i] * (num / denominations[i]) + " ");
+        // System.out.print(denominations[i] + "x" + num / denominations[i] + " = "+ denominations[i] * (num / denominations[i]) + " ");
         num = num % denominations[i];
         // System.out.println("=> remaining: " + num);
       }
@@ -27,7 +27,7 @@ public class IndianCoins {
   }
 
   public static void main(String[] args) {
-    int value = 121;
+    int value = 1059;
     System.out.println(countCoins(value));
   }
 }
