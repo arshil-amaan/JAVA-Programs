b6037724ce39ac530c6cca20e58e334fd49eecf8
Arshil <arshilamaanansari@gmail.com>
2024-04-16 02:34:02 +0530

    TCS Prep

--- Area_circle.java
+++ Area_circle.java
@@ -4,7 +4,7 @@ class Area_circle {
 	double r=10.5;
 	public static void main(String []args){
 		// creating object to access instance variable
-		area_circle myobj = new area_circle();
+		Area_circle myobj = new Area_circle();
 		// printing area of Circle
 		System.out.println("Area of Circle is :\n"+Math.pow(myobj.r,2)*22/7);
 	}
--- Area_rect.java
+++ Area_rect.java
@@ -4,7 +4,7 @@ class Area_rect {
 	int l=4, b=9;
 	public static void main(String []args){
 		// object created of same class
-		area_rect myobj = new area_rect();
+		Area_rect myobj = new Area_rect();
 		// printing area of rectangle
 		System.out.println("Area of Rectangle is :\n"+myobj.l*myobj.b);
 	}
