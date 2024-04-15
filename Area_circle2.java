e7e274f7ffafd4271c67271d964b7663bb28cef7
Arshil <arshilamaanansari@gmail.com>
2024-04-16 02:30:35 +0530

    TCS Prep

--- Area_circle2.java
+++ Area_circle2.java
@@ -2,11 +2,12 @@ import java.util.*;
 //code for area of circle (local Variable)
 class Area_circle2 {
 	public static void main(String[] args){
-		Scanner scs = new Scanner(System.in);
-		// local variable
-		System.out.println("Enter radius of the Circle.");
-		double r=scs.nextInt();
-		// printing area of Circle
-		System.out.println("Area of Circle is :\n"+Math.pow(r,2)*22/7);
+		try (Scanner scs = new Scanner(System.in)) {
+			// local variable
+			System.out.println("Enter radius of the Circle.");
+			double r=scs.nextInt();
+			// printing area of Circle
+			System.out.println("Area of Circle is :\n"+Math.pow(r,2)*22/7);
+		}
 	} 
 }
