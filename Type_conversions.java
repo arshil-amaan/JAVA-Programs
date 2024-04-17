329409d09649fd3fc89730a18dd1d923445cf90b
Arshil <arshilamaanansari@gmail.com>
2024-04-18 02:34:20 +0530

    ren: type conversions

--- Type_conversions.java
+++ Type_conversions.java
@@ -1,6 +1,6 @@
 // import java.util.*;
 
-public class test {
+public class Type_conversions {
     public static void main(String[] args) {
         // Scanner sc = new Scanner(System.in);
         int num = 1;
